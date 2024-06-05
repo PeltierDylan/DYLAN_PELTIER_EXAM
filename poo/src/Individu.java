a67fbce6ebf9111aac11adcc759c5c6567c6a5d7
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 11:28:17 +0200

    Ajout de la classe abstraite Individu.java

---
+++ poo/src/Individu.java
@@ -0,0 +1,39 @@
+package base;
+
+public abstract class Individu {
+    String nom;
+    String prenom;
+    int identifiant;
+
+    public Individu(String nom, String prenom, int identifiant) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.identifiant = identifiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public abstract void montrerDetails();
+}
