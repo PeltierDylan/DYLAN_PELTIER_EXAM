036b7d53fa15a3be10325342b78d0f92970fbafc
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 13:00:16 +0200

    Update Etudiant

--- poo/src/Etudiant.java
+++ poo/src/Etudiant.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Etudiant extends Individu {
     Date dateInscription;
     String niveauEtude;
+    ArrayList<Cours> coursList = new ArrayList<Cours>();
 
     public Etudiant(String nom, String prenom, int identifiant, Date dateInscription, String niveauEtude) {
         super(nom, prenom, identifiant);
@@ -17,5 +19,31 @@ public class Etudiant extends Individu {
                         "\nPrenom: " + this.prenom +
                         "\nDate d'inscription: " + this.dateInscription +
                         "\nNiveau d'étude: " + this.niveauEtude);
+    }
+
+    public void participer(Cours cours) {
+        if (!this.coursList.contains(cours)) {
+            System.out.println(
+                    "L'élève " + this.nom + " " + this.prenom +
+                            " participe au cours de " + cours.getTitre());
+            this.coursList.add(cours);
+            cours.placesRestantes--;
+        } else {
+            System.out.println("L'élève " + this.nom + " " + this.prenom +
+                    " participe déjà au cours de " + cours.getTitre());
+        }
+    }
+
+    public void quitter(Cours cours) {
+        if (!this.coursList.contains(cours)) {
+            System.out.println(
+                    "L'élève " + this.nom + " " + this.prenom +
+                            " a quitté le cours de " + cours.getTitre());
+            this.coursList.remove(cours);
+            cours.placesRestantes++;
+        } else {
+            System.out.println("L'élève " + this.nom + " " + this.prenom +
+                    " ne participe pas au cours de " + cours.getTitre());
+        }
     }
 }
