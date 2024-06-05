1110fca07f2d8d3168bf4808ae947ba351124ba0
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 13:01:51 +0200

    update

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,23 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
     @SuppressWarnings("deprecation")
     public static void main(String[] args) {
+        ArrayList<Individu> individus = new ArrayList<Individu>();
+        ArrayList<Cours> coursList = new ArrayList<Cours>();
 
-        Individu[] individus = new Individu[5];
-        individus[0] = new Enseignant("Fleury", "Sebastien", 1, "Mathématiques", 2000);
-        individus[1] = new Etudiant("Peltier", "Dylan", 2, new Date(2024, 2, 23), "Licence");
-        individus[2] = new Etudiant("Larde", "thomas", 1, new Date(2023, 9, 12), "Licence");
-        individus[3] = new Enseignant("Barbier", "Gael", 2, "Programmation", 1900);
-        individus[4] = new Etudiant("Florella", "Vincent", 3, new Date(2024, 9, 1), "BTS");
-        Cours cours1 = new Cours("Français", "Fleury", 2, 2);
+        individus.add(new Enseignant("Fleury", "Sebastien", 1, "Mathématiques", 2000));
+        individus.add(new Etudiant("Peltier", "Dylan", 2, new Date(2024, 2, 23), "Licence"));
+        individus.add(new Etudiant("Larde", "thomas", 1, new Date(2023, 9, 12), "Licence"));
+        individus.add(new Enseignant("Barbier", "Gael", 2, "Programmation", 1900));
+        individus.add(new Etudiant("Florella", "Vincent", 3, new Date(2024, 9, 1), "BTS"));
 
-        cours1.montrerDetails();
+        coursList.add(new Cours("Français", "Fleury", 2, 2));
+        coursList.add(new Cours("maTH", "Fleury", 2, 2));
+        coursList.add(new Cours("Prog", "Fleury", 2, 2));
+
         System.out.println();
 
-        for (int i = 0; i != individus.length; i++) {
-            individus[i].montrerDetails();
+        for (int i = 0; i != individus.size(); i++) {
+            individus.get(i).montrerDetails();
         }
+        for (int i = 0; i != coursList.size(); i++) {
+            coursList.get(i).montrerDetails();
+        }
+        individus.get(1).participer(coursList.get(0));
     }
 
 }
