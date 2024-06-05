9e861103203dfb0f078da718bda6f07b61c3232f
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 12:42:08 +0200

    Modification Main pour polymorphisme

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,14 +1,23 @@
-import java.sql.Date;
+import java.util.Date;
 
 public class Main {
     @SuppressWarnings("deprecation")
     public static void main(String[] args) {
-        Etudiant ind1 = new Etudiant("Peltier", "Dylan", 1, new Date(2023, 9, 1), "Licence");
-        Cours cours1 = new Cours("Français", "Fleury", 2, 2);
-        System.out.println();
-        ind1.montrerDetails();
-        cours1.montrerDetails();
 
+        Individu[] individus = new Individu[5];
+        individus[0] = new Enseignant("Fleury", "Sebastien", 1, "Mathématiques", 2000);
+        individus[1] = new Etudiant("Peltier", "Dylan", 2, new Date(2024, 2, 23), "Licence");
+        individus[2] = new Etudiant("Larde", "thomas", 1, new Date(2023, 9, 12), "Licence");
+        individus[3] = new Enseignant("Barbier", "Gael", 2, "Programmation", 1900);
+        individus[4] = new Etudiant("Florella", "Vincent", 3, new Date(2024, 9, 1), "BTS");
+        Cours cours1 = new Cours("Français", "Fleury", 2, 2);
+
+        cours1.montrerDetails();
+        System.out.println();
+
+        for (int i = 0; i != individus.length; i++) {
+            individus[i].montrerDetails();
+        }
     }
 
 }
