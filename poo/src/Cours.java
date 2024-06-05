826522c53abc74f533d3fe674a7094b3d6762e81
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 11:41:36 +0200

    Création de la classe Cours avec ses attributs

---
+++ poo/src/Cours.java
@@ -0,0 +1,10 @@
+public class Cours {
+    String titre;
+    String formateur;
+    int duree;// En heures
+    int placesRestantes;
+
+    public Cours(String titre, String formateur, int duree, int placesRestantes) {
+
+    }
+}
