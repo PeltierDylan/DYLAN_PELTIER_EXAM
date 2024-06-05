5ae49b1ef8759e4b595cf6419c46c60e7323aba1
Dylan Peltier <dylan.peltier.dp@gmail.com>
2024-06-05 11:43:09 +0200

    Ajout du constructeur et des getters/setters de la classe Cours

--- poo/src/Cours.java
+++ poo/src/Cours.java
@@ -5,6 +5,42 @@ public class Cours {
     int placesRestantes;
 
     public Cours(String titre, String formateur, int duree, int placesRestantes) {
-
+        this.titre = titre;
+        this.formateur = formateur;
+        this.duree = duree;
+        this.placesRestantes = placesRestantes;
     }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getFormateur() {
+        return formateur;
+    }
+
+    public void setFormateur(String formateur) {
+        this.formateur = formateur;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public int getPlacesRestantes() {
+        return placesRestantes;
+    }
+
+    public void setPlacesRestantes(int placesRestantes) {
+        this.placesRestantes = placesRestantes;
+    }
+
 }
